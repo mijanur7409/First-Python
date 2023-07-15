ca8aeb6d9b95058969ad2a1ff00e108fe593cfcc
mijanur7409 <mijanurrahman7409@gmail.com>
2023-07-15 13:24:52 +0600

    update py

--- first.py
+++ first.py
@@ -1,2 +1,11 @@
 
-print("Hello Mijanur")
+name = "Mijanur Rahman Sinha"
+roll = 450321
+depertment = "computer"
+
+
+print("student info:")
+print("----------------------------")
+print("Name: "+name)
+print("Roll: ",roll)
+print("Depertment: "+depertment)
